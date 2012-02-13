689173f12ad3ae61d8fc6087f2b14c9e1f07aca1
Thomas Garrood <thomas@sandboxdigital.com.au>
2012-02-14 10:25:34 +1100

    ajaxForm query plugin - submits form details to Tg_Form_Ajax

---
+++ js/tg/ajaxform.js
@@ -0,0 +1,46 @@
+
+$.fn.ajaxForm = function (options)
+{
+    var that = this;
+    var id = $(that).attr('id');
+    var end_url = $(this).attr('action');
+
+    options.onError = options.onError || function (){};
+
+    var errorBlock = $('#'+id+'Error');
+    if (errorBlock.length==0)
+    {
+        errorBlock = $('<div id="'+id+'Error"></div>').insertBefore(that);
+    }
+
+    function getHTML (errArray){
+        var h = '<div class="errors">';
+        $.each(errArray,function(elementId,errors){
+            $.each(errors,function(errorId,errorMsg){
+                h += '<p id="'+elementId+'-'+errorId+'" class="error">'+errorMsg+'</p>';
+            });
+        });
+        h += '</div>';
+        return h;
+    };
+
+    function doValidate (){
+        var url = end_url;
+        var data = $(that).serialize();
+        $.post(url,data,function(response){
+            if (response==true)
+                options.onValid();
+            else {
+                errorBlock.find('.errors').remove();
+                errorBlock.append(getHTML(response));
+                options.onError ();
+            }
+        },'json');
+    };
+
+    $(this).submit (function(){
+        doValidate();
+        return false;
+    })
+    return this;
+}
